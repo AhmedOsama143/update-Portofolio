5202077eeeb89f3ce102d0c3301719dab25a28b5
AhmedOsama143 <ao8911323@gmail.com>
2025-10-07 02:12:41 +0300

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/update-Portofolio/",
+  base: "/my-portfolio/",
   server: {
     historyApiFallback: true,
   },
