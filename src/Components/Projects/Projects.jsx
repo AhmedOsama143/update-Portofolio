959e455b394ec776f9c783cc4bdd685797a5ae9d
AhmedOsama143 <ao8911323@gmail.com>
2025-10-07 11:21:53 +0300

    Fix image paths for Netlify/GitHub Pages

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,21 +3,6 @@ import Card from "../Card/Card";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import FreshCartImg from "../../assets/imgs/FreshCart.png";
-import FokirImg from "../../assets/imgs/Fokkir.png";
-import DevfolioImg from "../../assets/imgs/devfoilo.png";
-import Daniels from "../../assets/imgs/Daniels.png";
-// import GameZone from "../../assets/imgs/GameZone.PNG";
-// import Yummy from "../../assets/imgs/Yummy.PNG";
-import Weather from "../../assets/imgs/Weather.PNG";
-import Fashion from "../../assets/imgs/Fashion.png";
-import Tomato from "../../assets/imgs/Tomato.png";
-import Iphone from "../../assets/imgs/Iphone.png";
-import Mealify from "../../assets/imgs/Mealify.PNG";
-import SimpleEcommerce from "../../assets/imgs/simple E-Commerce.png";
-// import Bookmarker from "../../assets/imgs/Bookmarker.PNG";
-// import SmartLogin from "../../assets/imgs/SmartLogin.png";
-// import QuotesGenerator from "../../assets/imgs/QuotesGenerator.png";
 import FloatingFAIcons from "../BubblesBackground/FloatingFAIcons";
 import { faCode } from "@fortawesome/free-solid-svg-icons";
 import {
@@ -32,25 +17,24 @@ export default function Projects() {
   useEffect(() => {
     AOS.init({ duration: 300, once: false, offset: 100 });
   }, []);
+
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   const categories = ["All", "CSS & HTML", "JavaScript", "React"];
-  // const categories = ["All", "CSS & HTML", "JavaScript", "React","Next.js"];
 
   const projects = [
     {
       title: "Fresh Cart",
-      img: FreshCartImg,
+      img: "/imgs/FreshCart.png",
       liveDemo: "https://fresh-cart-psi-five.vercel.app/signin",
       repo: "https://github.com/AhmedOsama143/FreshCart",
       description:
         "FreshCart is a fast and modern online store for fresh groceries and essentials. Enjoy easy browsing, secure checkout, and quick delivery to your door.",
       category: "React",
     },
-
     {
       title: "simple E-commerce",
-      img: SimpleEcommerce,
+      img: "/imgs/simple-E-Commerce.png",
       liveDemo: "https://ahmedosama143.github.io/simple-E-Commerce/",
       repo: "https://github.com/AhmedOsama143/simple-E-Commerce",
       description:
@@ -59,7 +43,7 @@ export default function Projects() {
     },
     {
       title: "Fashion",
-      img: Fashion,
+      img: "/imgs/Fashion.png",
       liveDemo: "https://fashion-five-lac.vercel.app/",
       repo: "https://github.com/AhmedOsama143/fashion",
       description:
@@ -68,7 +52,7 @@ export default function Projects() {
     },
     {
       title: "Fokir",
-      img: FokirImg,
+      img: "/imgs/Fokkir.png",
       liveDemo: "https://fokir-rho.vercel.app/",
       repo: "https://github.com/AhmedOsama143/Fokir",
       description:
@@ -77,16 +61,16 @@ export default function Projects() {
     },
     {
       title: "Iphone 15 Model",
-      img: Iphone,
+      img: "/imgs/Iphone.png",
       liveDemo: "https://iphone-model-ahmed-osama.vercel.app/",
       repo: "https://github.com/AhmedOsama143/Iphone-Model",
       description:
-        "Iphone 15 Model is a responsive product landing page built with React and Simulates reality, showcasing the latest iPhone features and design.",
+        "Iphone 15 Model is a responsive product landing page built with React and simulates reality, showcasing the latest iPhone features and design.",
       category: "React",
     },
     {
       title: "Devfolio",
-      img: DevfolioImg,
+      img: "/imgs/devfoilo.png",
       liveDemo: "https://dev-folio-25yd.vercel.app/",
       repo: "https://github.com/AhmedOsama143/DevFolio",
       description:
@@ -95,7 +79,7 @@ export default function Projects() {
     },
     {
       title: "Tomato",
-      img: Tomato,
+      img: "/imgs/Tomato.png",
       liveDemo: "https://food-delivery-rho-sand.vercel.app/",
       repo: "https://github.com/AhmedOsama143/Food-delivery",
       description:
@@ -104,77 +88,31 @@ export default function Projects() {
     },
     {
       title: "Daniels",
-      img: Daniels,
+      img: "/imgs/Daniels.png",
       liveDemo: "https://daniels-orpin.vercel.app/",
       repo: "https://github.com/AhmedOsama143/DANIELS",
       description:
         "A modern and responsive personal portfolio template designed for developers, freelancers, or creatives.",
       category: "CSS & HTML",
     },
-    // {
-    //   title: "GameZone",
-    //   img: GameZone,
-    //   liveDemo: "https://mahmoudosman2611.github.io/Game-Zone/",
-    //   repo: "https://github.com/mahmoudosman2611/Game-Zone",
-    //   description:
-    //     "Game Zone is a free gaming website built using HTML, CSS, and Vanilla JavaScript.",
-    //   category: "JavaScript",
-    // },
-    // {
-    //   title: "Yummy",
-    //   img: Yummy,
-    //   liveDemo: "https://mahmoudosman2611.github.io/Yummy/",
-    //   repo: "https://github.com/mahmoudosman2611/Yummy",
-    //   description:
-    //     "Yummy is a website that offers delicious and healthy recipes in a simple way.",
-    //   category: "JavaScript",
-    // },
     {
       title: "Weather-app",
-      img: Weather,
+      img: "/imgs/Weather.PNG",
       liveDemo: "https://ahmedosama143.github.io/weather/",
       repo: "https://github.com/AhmedOsama143/weather",
       description:
         "Get accurate, real-time weather updates for any city worldwide.",
       category: "JavaScript",
     },
-
     {
       title: "Mealify",
-      img: Mealify,
+      img: "/imgs/Mealify.PNG",
       liveDemo: "https://mealify-lac.vercel.app/",
       repo: "https://github.com/AhmedOsama143/Mealify",
       description:
         "Mealify is a web application that allows users to search for recipes based on their ingredients.",
+      category: "JavaScript",
     },
-
-    // {
-    //   title: "SmartLogin",
-    //   img: SmartLogin,
-    //   liveDemo: "https://mahmoudosman2611.github.io/Simple-smart-login/",
-    //   repo: "https://github.com/mahmoudosman2611/Simple-smart-login",
-    //   description:
-    //     "A responsive login system built with HTML, CSS, and JavaScript.",
-    //   category: "JavaScript",
-    // },
-    // {
-    //   title: "Bookmarker",
-    //   img: Bookmarker,
-    //   liveDemo: "https://mahmoudosman2611.github.io/Bookmark/",
-    //   repo: "https://github.com/mahmoudosman2611/Bookmark",
-    //   description:
-    //     "Bookmark Manager is a fast and responsive web app for saving and organizing your favorite websites.",
-    //   category: "JavaScript",
-    // },
-    // {
-    //   title: "Quotes Generator",
-    //   img: QuotesGenerator,
-    //   liveDemo: "https://mahmoudosman2611.github.io/simple-quotes-generator/",
-    //   repo: "https://github.com/mahmoudosman2611/simple-quotes-generator",
-    //   description:
-    //     "Simple Quotes Generator is a clean and responsive web app that displays a random quote.",
-    //   category: "JavaScript",
-    // },
   ];
 
   const filteredProjects =
@@ -195,6 +133,7 @@ export default function Projects() {
         duration={[12, 20]}
         sway={22}
       />
+
       <div
         className="pointer-events-none absolute inset-0 opacity-10"
         style={{
@@ -244,7 +183,7 @@ export default function Projects() {
               key={project.title}
               data-aos="fade-up"
               data-aos-delay={index * 100}
-              className="relative  rounded-2xl border border-white/10 bg-white/5 p-1 backdrop-blur-md
+              className="relative rounded-2xl border border-white/10 bg-white/5 p-1 backdrop-blur-md
                           shadow-[0_0_24px_rgba(59,130,246,0.08)]
                           transition-all duration-300 hover:-translate-y-0.5 hover:shadow-[0_0_34px_rgba(59,130,246,0.18)]"
             >
