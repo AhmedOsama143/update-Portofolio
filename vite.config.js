8a39213322da4d09a489b8f24968977ac4bf1f03
AhmedOsama143 <ao8911323@gmail.com>
2025-10-07 11:06:38 +0300

    update vite config for netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/update-Portofolio/",
+  base: "./",
   build: {
     outDir: "dist",
   },
